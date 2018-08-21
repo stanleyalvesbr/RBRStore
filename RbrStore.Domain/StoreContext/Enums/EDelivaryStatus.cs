dda2f667bfe2766de22db4203b44eb9dca73fb6d
Stanley <stanley.alv3s@gmail.com>
2018-08-21 09:55:22 -0300

    Criação de Status

---
+++ RbrStore.Domain/StoreContext/Enums/EDelivaryStatus.cs
@@ -0,0 +1,14 @@
+using System;
+using RbrStore.Domain.StoreContext.Enums;
+
+namespace RbrStore.Domain.StoreContext.Enums
+{
+   public enum EDelivaryStatus
+   {
+       Waiting = 1,
+       Shipped = 2,
+       Delivered = 3,
+      Canceled = 4
+
+    }
+}
